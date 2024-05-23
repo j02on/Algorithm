a1730e445e213fb735567e246a924de6c1786ebb
박지연 <163213733+j02on@users.noreply.github.com>
2024-05-23 22:29:25 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	
+	scanf("%d", &a);
+	
+	for(int i = 1; i <= a; i++)
+	{
+		for(int h = i; h < a; h++)
+		{
+			printf(" ");
+		}
+		for(int j = 0; j < i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
