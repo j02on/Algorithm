2973ab8534c906a8a6da98f727448f816d3ebd48
박지연 <163213733+j02on@users.noreply.github.com>
2024-05-23 22:14:26 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	
+	scanf("%d", &a);
+	
+	if(90 <= a)
+	{
+		printf("A");
+	}
+	else if(80 <= a)
+	{
+		printf("B");
+	}
+	else if(70 <= a)
+	{
+		printf("C");
+	}
+	else if(60 <= a)
+	{
+		printf("D");
+	}
+	else
+	{
+		printf("F");
+	}
+	return 0;
+}
