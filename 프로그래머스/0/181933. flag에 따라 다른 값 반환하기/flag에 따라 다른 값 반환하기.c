dee035cb676a2ad33995360f4612684438f93339
박지연 <163213733+j02on@users.noreply.github.com>
2024-10-04 18:20:59 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int a, int b, bool flag) {
+    int answer = 0;
+    if(flag == true)
+    {
+        answer = a + b;
+    }
+    else if(flag == false)
+    {
+        answer = a - b;
+    }
+    return answer;
+}
