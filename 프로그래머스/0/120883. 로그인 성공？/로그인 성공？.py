53bbae8506d5a76099fe6ede58eb97a1b46aa245
박지연 <163213733+j02on@users.noreply.github.com>
2025-08-11 10:17:45 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,9 @@
+def solution(id_pw, db):
+    answer = 0
+    for i in db :
+        if id_pw[0] == i[0] :
+            if(id_pw[1] == i[1]) :
+                return "login"
+            else :
+                return "wrong pw"
+    return "fail"
